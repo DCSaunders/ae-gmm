a8a492e4b52a298d14c949d87c5f5c53746436c9
DCSaunders <ds636@cam.ac.uk>
2017-01-14 19:32:10 +0000

    walkthrough updated var names

--- adversarial_mnist.py
+++ adversarial_mnist.py
@@ -14,29 +14,36 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 FLAGS = None
+# Data distribution
 mu,sigma=-1,1
 xs=np.linspace(-5,5,1000)
 plt.plot(xs, norm.pdf(xs,loc=mu,scale=sigma))
+
 TRAIN_ITERS=10000
 M=200 # minibatch size
+in_size=1 # Size of data in
+out_size=1 # Size of data out
 
-# MLP - used for D_pre, D1, D2, G networks
+# MLP - used for D_pre, D_true, D_fake, G networks
 def mlp(input, output_dim):
+    hidden_1 = 200
+    hidden_2 = 100
     # construct learnable parameters within local scope
-    w1=tf.get_variable("w0", [input.get_shape()[1], 6], initializer=tf.random_normal_initializer())
-    b1=tf.get_variable("b0", [6], initializer=tf.constant_initializer(0.0))
-    w2=tf.get_variable("w1", [6, 5], initializer=tf.random_normal_initializer())
-    b2=tf.get_variable("b1", [5], initializer=tf.constant_initializer(0.0))
-    w3=tf.get_variable("w2", [5,output_dim], initializer=tf.random_normal_initializer())
+    w1=tf.get_variable("w0", [input.get_shape()[1], hidden_1],
+                       initializer=tf.random_normal_initializer())
+    b1=tf.get_variable("b0", [hidden_1], initializer=tf.constant_initializer(0.0))
+    w2=tf.get_variable("w1", [hidden_1, hidden_2], initializer=tf.random_normal_initializer())
+    b2=tf.get_variable("b1", [hidden_2], initializer=tf.constant_initializer(0.0))
+    w3=tf.get_variable("w2", [hidden_2,output_dim], initializer=tf.random_normal_initializer())
     b3=tf.get_variable("b2", [output_dim], initializer=tf.constant_initializer(0.0))
     # nn operators
-    fc1=tf.nn.tanh(tf.matmul(input,w1)+b1)
-    fc2=tf.nn.tanh(tf.matmul(fc1,w2)+b2)
-    fc3=tf.nn.tanh(tf.matmul(fc2,w3)+b3)
-    return fc3, [w1,b1,w2,b2,w3,b3]
+    out_1=tf.nn.tanh(tf.matmul(input,w1)+b1)
+    out_2=tf.nn.tanh(tf.matmul(out_1,w2)+b2)
+    out_3=tf.nn.tanh(tf.matmul(out_2,w3)+b3)
+    return out_3, [w1,b1,w2,b2,w3,b3]
 
 # re-used for optimizing all networks
-def momentum_optimizer(loss,var_list):
+def momentum_optimizer(loss, weights):
     batch = tf.Variable(0)
     learning_rate = tf.train.exponential_decay(
         0.001,                # Base learning rate.
@@ -44,8 +51,8 @@ def momentum_optimizer(loss,var_list):
         TRAIN_ITERS // 4,          # Decay step - this decays 4 times throughout training process.
         0.95,                # Decay rate.
         staircase=True)
-    #optimizer=tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=batch,var_list=var_list)
-    optimizer=tf.train.MomentumOptimizer(learning_rate,0.6).minimize(loss,global_step=batch,var_list=var_list)
+    optimizer=tf.train.MomentumOptimizer(learning_rate,0.6).minimize(
+        loss, global_step=batch, var_list=weights)
     return optimizer
 
 # plot decision surface
@@ -82,7 +89,7 @@ def plot_fig():
     # process multiple points in parallel in same minibatch
     for i in range(int(r/M)):
         x=np.reshape(xs[M*i:M*(i+1)],(M,1))
-        ds[M*i:M*(i+1)]=sess.run(D1,{x_node: x})
+        ds[M*i:M*(i+1)]=sess.run(D_true,{x_node: x})
 
     ax.plot(xs, ds, label='decision boundary')
 
@@ -100,70 +107,73 @@ def plot_fig():
     plt.legend()
     plt.show()
 
+def noise_prior_sample(batch_size, upper, lower, scale):
+    return np.linspace(lower, upper, batch_size) + np.random.random(batch_size) * scale
 
+# Pretraining for faster convergence
 with tf.variable_scope("D_pre"):
-    input_node=tf.placeholder(tf.float32, shape=(M,1))
-    train_labels=tf.placeholder(tf.float32,shape=(M,1))
-    D,theta=mlp(input_node,1)
-    loss=tf.reduce_mean(tf.square(D-train_labels))
+    input_node=tf.placeholder(tf.float32, shape=(M,in_size))
+    train_labels=tf.placeholder(tf.float32,shape=(M,out_size))
+    D_pre, weights_pre=mlp(input_node,out_size)
+    loss=tf.reduce_mean(tf.square(D_pre - train_labels))
 
 optimizer=momentum_optimizer(loss,None)
 sess=tf.InteractiveSession()
 tf.initialize_all_variables().run()
-plot_d0(D,input_node)
+plot_d0(D_pre, input_node)
 plt.title('Initial Decision Boundary')
 
 lh=np.zeros(1000)
 for i in range(1000):
-    #d=np.random.normal(mu,sigma,M)
-    d=(np.random.random(M)-0.5) * 10.0 # instead of sampling only from gaussian, want the domain to be covered as uniformly as possible
-    labels=norm.pdf(d,loc=mu,scale=sigma)
-    lh[i],_=sess.run([loss,optimizer], {input_node: np.reshape(d,(M,1)), train_labels: np.reshape(labels,(M,1))})
+    d = (np.random.random(M)-0.5) * 10.0 # instead of sampling only from gaussian, want the domain to be covered as uniformly as possible
+    labels = norm.pdf(d,loc=mu,scale=sigma)
+    lh[i], _ =sess.run([loss,optimizer], {input_node: np.reshape(d,(M,in_size)), train_labels: np.reshape(labels,(M,out_size))})
 
 plt.plot(lh)
 plt.title('Training Loss')
-plot_d0(D,input_node)
-weightsD=sess.run(theta)
+plot_d0(D_pre,input_node)
+pretrained = sess.run(weights_pre)
 sess.close()
 
-with tf.variable_scope("G"):
-    z_node=tf.placeholder(tf.float32, shape=(M,1)) # M uniform01 floats
-    G,theta_g=mlp(z_node,1) # generate normal transformation of Z
+with tf.variable_scope("Gen"):
+    z_node=tf.placeholder(tf.float32, shape=(M,in_size)) # M uniform01 floats
+    G,weights_gen=mlp(z_node,in_size) # generate normal transformation of Z - output size must match input size for D
     G=tf.mul(5.0,G) # scale up by 5 to match range
-with tf.variable_scope("D") as scope:
+with tf.variable_scope("Discr") as scope:
     # D(x)
-    x_node=tf.placeholder(tf.float32, shape=(M,1)) # input M normally distributed floats
-    fc,theta_d=mlp(x_node,1) # output likelihood of being normally distributed
-    D1=tf.maximum(tf.minimum(fc,.99), 0.01) # clamp as a probability
+    x_node=tf.placeholder(tf.float32, shape=(M,in_size)) # input M normally distributed floats
+    out_prob, weights_discr=mlp(x_node, out_size) # output likelihood of being normally distributed
+    D_true = tf.maximum(tf.minimum(out_prob, .99), 0.01) # clamp as a probability
     # make a copy of D that uses the same variables, but takes in G as input
     scope.reuse_variables()
-    fc,theta_d=mlp(G,1)
-    D2=tf.maximum(tf.minimum(fc,.99), 0.01)
-obj_d=tf.reduce_mean(tf.log(D1)+tf.log(1-D2))
-obj_g=tf.reduce_mean(tf.log(D2))
+    out_prob, weights_discr = mlp(G, in_size)
+    D_fake = tf.maximum(tf.minimum(out_prob, .99), 0.01)
+obj_discr=tf.reduce_mean(tf.log(D_true) + tf.log(1 - D_fake))
+obj_gen=tf.reduce_mean(tf.log(D_fake))
 
 # set up optimizer for G,D
-opt_d=momentum_optimizer(1-obj_d, theta_d)
-opt_g=momentum_optimizer(1-obj_g, theta_g) # maximize log(D(G(z)))
+opt_d = momentum_optimizer(1-obj_discr, weights_discr)
+opt_g = momentum_optimizer(1-obj_gen, weights_gen) # maximize log(D(G(z)))
 
-sess=tf.InteractiveSession()
+sess = tf.InteractiveSession()
 tf.initialize_all_variables().run()
 # copy weights from pre-training over to new D network
-for i,v in enumerate(theta_d):
-    sess.run(v.assign(weightsD[i]))
+for i, w in enumerate(weights_discr):
+    sess.run(w.assign(pretrained[i]))
 plot_fig()
 plt.title('Before Training')
 # Algorithm 1 of Goodfellow et al 2014
-k=1
+k=1 # Iterations of discriminator for every generator iteration
 histd, histg= np.zeros(TRAIN_ITERS), np.zeros(TRAIN_ITERS)
 for i in range(TRAIN_ITERS):
     for j in range(k):
-        x= np.random.normal(mu,sigma,M) # sampled m-batch from p_data
+        x = np.random.normal(mu,sigma,M) # sampled m-batch from p_data
         x.sort()
-        z= np.linspace(-5.0,5.0,M)+np.random.random(M)*0.01  # sample m-batch from noise prior
-        histd[i],_=sess.run([obj_d,opt_d], {x_node: np.reshape(x,(M,1)), z_node: np.reshape(z,(M,1))})
-    z= np.linspace(-5.0,5.0,M)+np.random.random(M)*0.01 # sample noise prior
-    histg[i],_=sess.run([obj_g,opt_g], {z_node: np.reshape(z,(M,1))}) # update generator
+        z = noise_prior_sample(M, lower=-5, upper=5, scale=0.01)  # sample m-batch from noise prior
+        histd[i], _ =sess.run([obj_discr, opt_d], 
+                              {x_node: np.reshape(x,(M,in_size)), z_node: np.reshape(z,(M,in_size))})
+    z = noise_prior_sample(M, lower=-5, upper=5, scale=0.01)
+    histg[i], _ =sess.run([obj_g, opt_g], {z_node: np.reshape(z, (M, in_size))}) # update generator
     if i % (TRAIN_ITERS//10) == 0:
         print(float(i)/float(TRAIN_ITERS))
 plt.plot(range(TRAIN_ITERS),histd, label='obj_d')
